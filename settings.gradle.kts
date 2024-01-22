9065f227e4a88a63473b67e38ad0ac2b2326f0f7
juanfernandoro@ufps.edu.co <juanfernandoro@ufps.edu.co>
2024-01-22 18:59:43 -0500

    Push new project with its modules

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Gym Evolution"
+include(":app")
+include(":feature")
+include(":core")
+include(":core:domain")
+include(":core:data")
