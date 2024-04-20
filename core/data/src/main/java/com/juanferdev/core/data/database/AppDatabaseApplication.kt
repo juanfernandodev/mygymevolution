b2bbf0e0a018bd68a683d466ce7c321aaf0c9fb8
juanferdev <jfromero1367@gmail.com>
2024-04-19 20:21:54 -0500

    Removing files from core because they are not necessaries.

--- core/data/src/main/java/com/juanferdev/core/data/database/AppDatabaseApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.juanferdev.core.data.database
-
-import android.app.Application
-
-class AppDatabaseApplication : Application() {
-    val database: AppDatabase by lazy { AppDatabase.instance(this) }
-}
