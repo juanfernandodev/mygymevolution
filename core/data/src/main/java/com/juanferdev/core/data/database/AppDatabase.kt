6d5543d19be1de347b20539f9818b5cfe1134812
juanferdev <jfromero1367@gmail.com>
2024-04-21 18:46:12 -0500

    Change database version

--- core/data/src/main/java/com/juanferdev/core/data/database/AppDatabase.kt
+++ core/data/src/main/java/com/juanferdev/core/data/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.juanferdev.core.data.musclegroup.datastores.MuscleGroupDao
 import com.juanferdev.core.data.musclegroup.models.MuscleGroupModel
 
-@Database(entities = [MuscleGroupModel::class], version = 0)
+@Database(entities = [MuscleGroupModel::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun muscleGroupDao(): MuscleGroupDao
