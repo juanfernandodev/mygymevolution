25f046e2a72e44dbf480ef36cb0a4bc03528d760
juanferdev <jfromero1367@gmail.com>
2024-04-21 19:27:14 -0500

    Improving the Add Muscle group screen

--- feature/src/main/java/com/juanferdev/feature/addmusclegroup/screen/AddMuscleGroupScreen.kt
+++ feature/src/main/java/com/juanferdev/feature/addmusclegroup/screen/AddMuscleGroupScreen.kt
@@ -2,30 +2,27 @@ package com.juanferdev.feature.addmusclegroup.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.juanferdev.core.data.musclegroup.repositories.LocalStoreStatus
 import com.juanferdev.feature.R
 import com.juanferdev.feature.addmusclegroup.stateholder.AddMuscleGroupViewModel
 import com.juanferdev.feature.composables.ErrorDialog
 import com.juanferdev.feature.features.composables.LoadingWheel
+import com.juanferdev.feature.features.home.ui.theme.MyGymEvolutionTheme
 
 
 @Composable
@@ -52,11 +49,14 @@ fun RegisterMuscleGroup(
 ) {
     val nameMuscleGroup = remember { mutableStateOf(String()) }
     Column(
-        modifier = Modifier.padding(24.dp),
-        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextField(
+        OutlinedTextField(
             value = nameMuscleGroup.value,
             onValueChange = { newValue ->
                 nameMuscleGroup.value = newValue
@@ -66,21 +66,6 @@ fun RegisterMuscleGroup(
                 .heightIn(56.dp)
                 .padding(bottom = 24.dp)
         )
-        Text(
-            "Select icon:",
-            fontSize = 14.sp,
-            modifier = Modifier
-                .align(Alignment.Start)
-                .padding(bottom = 24.dp)
-        )
-        Button(
-            onClick = { }, modifier = Modifier
-                .padding(bottom = 24.dp)
-                .width(128.dp)
-                .height(128.dp)
-        ) {
-            Icon(imageVector = Icons.Filled.Add, contentDescription = null)
-        }
         OutlinedButton(
             onClick = { onClickSave(nameMuscleGroup.value) },
             modifier = Modifier.padding(bottom = 24.dp)
@@ -88,4 +73,14 @@ fun RegisterMuscleGroup(
             Text(stringResource(R.string.save))
         }
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    MyGymEvolutionTheme {
+        RegisterMuscleGroup {}
+    }
+
 }
