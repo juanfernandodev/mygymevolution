c7a09cdb6c2f354a5a0160de498150dd413e9e45
juanferdev <jfromero1367@gmail.com>
2024-04-28 16:02:05 -0500

    Adding @Dao annotation and ExerciseModel in entities

--- core/data/src/main/java/com/juanferdev/core/data/database/AppDatabase.kt
+++ core/data/src/main/java/com/juanferdev/core/data/database/AppDatabase.kt
@@ -3,12 +3,16 @@ package com.juanferdev.core.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.juanferdev.core.data.exercise.datastores.ExerciseDAO
+import com.juanferdev.core.data.exercise.models.ExerciseModel
 import com.juanferdev.core.data.modality.datastores.ModalityDao
 import com.juanferdev.core.data.modality.models.ModalityModel
 import com.juanferdev.core.data.musclegroup.datastores.MuscleGroupDao
 import com.juanferdev.core.data.musclegroup.models.MuscleGroupModel
 
-@Database(entities = [MuscleGroupModel::class, ModalityModel::class], version = 1)
+@Database(
+    entities = [MuscleGroupModel::class, ModalityModel::class, ExerciseModel::class],
+    version = 1
+)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun muscleGroupDao(): MuscleGroupDao
--- core/data/src/main/java/com/juanferdev/core/data/exercise/datastores/ExerciseDAO.kt
+++ core/data/src/main/java/com/juanferdev/core/data/exercise/datastores/ExerciseDAO.kt
@@ -1,10 +1,12 @@
 package com.juanferdev.core.data.exercise.datastores
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.juanferdev.core.data.exercise.models.ExerciseModel
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface ExerciseDAO {
 
     @Query("SELECT * FROM ExerciseModel WHERE muscleGroupId = :muscleGroupId")
