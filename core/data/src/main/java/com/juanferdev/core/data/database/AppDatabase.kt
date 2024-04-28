c3f846c6d5444c31e81f8e94375890ccda7e7698
juanferdev <jfromero1367@gmail.com>
2024-04-28 15:58:13 -0500

    Providing ExerciseDao

--- core/data/src/main/java/com/juanferdev/core/data/database/AppDatabase.kt
+++ core/data/src/main/java/com/juanferdev/core/data/database/AppDatabase.kt
@@ -2,6 +2,7 @@ package com.juanferdev.core.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.juanferdev.core.data.exercise.datastores.ExerciseDAO
 import com.juanferdev.core.data.modality.datastores.ModalityDao
 import com.juanferdev.core.data.modality.models.ModalityModel
 import com.juanferdev.core.data.musclegroup.datastores.MuscleGroupDao
@@ -13,5 +14,7 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun muscleGroupDao(): MuscleGroupDao
 
     abstract fun modalityDao(): ModalityDao
+
+    abstract fun exerciseDao(): ExerciseDAO
 
 }
--- core/data/src/main/java/com/juanferdev/core/data/di/AppDataBaseModule.kt
+++ core/data/src/main/java/com/juanferdev/core/data/di/AppDataBaseModule.kt
@@ -4,6 +4,7 @@ package com.juanferdev.core.data.di
 import android.content.Context
 import androidx.room.Room
 import com.juanferdev.core.data.database.AppDatabase
+import com.juanferdev.core.data.exercise.datastores.ExerciseDAO
 import com.juanferdev.core.data.modality.datastores.ModalityDao
 import com.juanferdev.core.data.musclegroup.datastores.MuscleGroupDao
 import dagger.Module
@@ -34,5 +35,10 @@ object DatabaseModule {
     @Provides
     fun provideModalityDao(appDatabase: AppDatabase): ModalityDao {
         return appDatabase.modalityDao()
+    }
+
+    @Provides
+    fun provideExerciseDao(appDatabase: AppDatabase): ExerciseDAO {
+        return appDatabase.exerciseDao()
     }
 }
